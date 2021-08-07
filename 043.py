63f5caa6f734ed6749fa1e34bffd1a7032a2a18e
Kadir Burak Gündüz <kburakgndz@gmail.com>
2021-08-07 04:32:27 +0300

    43

--- 043.py
+++ 043.py
@@ -2,13 +2,13 @@ from itertools import permutations
 
 
 def d(number):
-    if int(number[1] + number[2] + number[3]) % 2 == 0:
-        if int(number[2] + number[3] + number[4]) % 3 == 0:
-            if int(number[3] + number[4] + number[5]) % 5 == 0:
-                if int(number[4] + number[5] + number[6]) % 7 == 0:
-                    if int(number[5] + number[6] + number[7]) % 11 == 0:
-                        if int(number[6] + number[7] + number[8]) % 13 == 0:
-                            if int(number[7] + number[8] + number[9]) % 17 == 0:
+    if int(number[:3]) % 2 == 0:
+        if int(number[2:5]) % 3 == 0:
+            if int(number[3:6]) % 5 == 0:
+                if int(number[4:7]) % 7 == 0:
+                    if int(number[5:8]) % 11 == 0:
+                        if int(number[6:9]) % 13 == 0:
+                            if int(number[7:]) % 17 == 0:
                                 return True
     return False
 
